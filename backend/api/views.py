e95f740951c8d9dfbd6545d2aebcc11748a77d68
CFE <hello@teamcfe.com>
2022-02-11 14:54:48 -0600

    7 - Echo GET Data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,21 @@
+import json
 from django.http import JsonResponse
 
-
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there, this is your Django API response!!"})
+    # request -> HttpRequest -> Django
+    # print(dir(request))
+    # request.body
+    print(request.GET) # url query params
+    print(request.POST)
+    body = request.body # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) # string of JSON data -> Python Dict
+    except:
+        pass
+    print(data)
+    # data['headers'] = request.headers # request.META ->
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,12 +4,12 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/" #http://127.0.0.1:8000/ 
 
-get_response = requests.get(endpoint, json={"query": "Hello world"}) # HTTP Request
-print(get_response.text) # print raw text response
-print(get_response.status_code)
+get_response = requests.get(endpoint, json={"product_id": 123 }) # HTTP Request
+# print(get_response.text) # print raw text response
+# print(get_response.status_code)
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Nototion ~ Python Dict
-print(get_response.json()['message'])
+print(get_response.json())
 # print(get_response.status_code)
