be773b652c483c2f6884cabfe0efda5acf711a40
CFE <hello@teamcfe.com>
2022-02-16 15:21:00 -0600

    21 - Custom Permissions

---
+++ backend/products/permissions.py
@@ -0,0 +1,33 @@
+from rest_framework import permissions
+
+
+class IsStaffEditorPermission(permissions.DjangoModelPermissions):
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+    # def has_permission(self, request, view):
+    #     if not request.user.is_staff:
+    #         return False
+    #     return super().has_permission(request, view)
+
+    # def has_permission(self, request, view):
+    #     user = request.user
+    #     print(user.get_all_permissions())
+    #     if user.is_staff:
+    #         if user.has_perm("products.add_product"):
+    #             return True
+    #         if user.has_perm("products.delete_product"):
+    #             return True
+    #         if user.has_perm("products.change_product"):
+    #             return True
+    #         if user.has_perm("products.view_product"):
+    #             return True
+    #         return False
+    #     return False
--- backend/products/views.py
+++ backend/products/views.py
@@ -5,13 +5,14 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from .models import Product
+from .permissions import IsStaffEditorPermission
 from .serializers import ProductSerializer
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.DjangoModelPermissions]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
